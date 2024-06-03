3287f08537187a6b032b0b605e74a6a1ad3a0b86
SatyaSravya2428 <144446795+SatyaSravya2428@users.noreply.github.com>
2024-06-03 08:41:58 +0530

    Update UploadImage.py

--- UploadImage.py
+++ UploadImage.py
@@ -10,22 +10,37 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ["table_name"])
 
 def lambda_handler(event, context):
-    print(event)
-    image_data = base64.b64decode(event['image'])
+    if 'content' not in event:
+        return{
+            'statusCode': 500,
+            'message': 'Please upload a image file',
+            'imageid' :''
+            }
+
+    image_data = base64.b64decode(event['content'])
     image_id = str(uuid.uuid4())
+    
     s3.put_object(Bucket=os.environ["bucket_name"],
                     Key=image_id,
                     Body=image_data
                     )
     
-    metadata = event['metadata']
+    metadata = event['metadata'] if 'metadata' in event else {'type': 'image uploaded from testing'}
     item = {'imageid': image_id,
             'timestamp': str(date.today()),
             'metadata': metadata
     }
-    table.put_item(Item=item)
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'imageId': image_id})
-    }
+    try:
+        table.put_item(Item=item)
+        
+        return {
+            'statusCode': 200,
+            'message': 'Successfully uploaded the image!',
+            'imageid': image_id
+        }
+    except:
+        return{
+            'statusCode': 500,
+            'message': 'Failed to update metdata in database',
+            'imageid' :''
+        }
