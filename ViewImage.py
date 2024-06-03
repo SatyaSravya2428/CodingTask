05f019d8eb0ee6b77518718fcd0971299bf7b47f
SatyaSravya2428 <144446795+SatyaSravya2428@users.noreply.github.com>
2024-06-03 08:42:38 +0530

    Added Exception handling

--- ViewImage.py
+++ ViewImage.py
@@ -5,12 +5,27 @@ import os
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    print("*******", event)
     image_id = event['pathParameters']['imageid']
-    s3_response = s3.get_object(Bucket=os.environ['bucket_name'], Key=image_id)
+    try:
+        s3_response = s3.get_object(Bucket=os.environ['bucket_name'], Key=image_id)
+    except s3.exceptions.NoSuchKey:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({'message': 'File not found'})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': str(e)})
+        }
     image_data = base64.b64encode(s3_response['Body'].read()).decode('utf-8')
     
     return {
         'statusCode': 200,
-        'body': json.dumps({'imageId': image_id, 'image': image_data})
+        'headers': {
+            "Content-Type": "application/jpg",
+            "Content-Disposition": "attachment; filename- {}".format(image_id)
+        },
+        'isBase64Encoded': True,
+        'body': image_data
     }
